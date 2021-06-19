620cda4eebe165bc4d0c5b27ee5f3e6d7e192225
nguobadia <nicoleguob@gmail.com>
2021-06-18 23:40:58 -0700

    Moved the custom_models directory to the approrpiate location

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nclgbd/PyTorch-Utilities",
-    packages=find_packages(),
+    packages=find_packages(include=["custom_models","pytorch_vision_utils",]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
