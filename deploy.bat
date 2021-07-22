89c57017dbc9309edb61f4909072c9e22ae15a9b
nguobadia <nicoleguob@gmail.com>
2021-07-21 19:22:03 -0700

    Added a deploy.bat file. Rebuilt the binaries.

--- deploy.bat
+++ deploy.bat
@@ -8,6 +8,9 @@ pip install -r requirements.txt
 cd ..
 
 pip uninstall pytorch_vision_utils -y
-bumpversion --dry-run --allow-dirty patch
+::bumpversion patch
 py -m build
 pip install dist/pytorch_vision_utils-0.3.2.tar.gz
+
+py -m pip install --upgrade twine
+py -m twine upload dist/pytorch_vision_utils-0.3.2.tar.gz --verbose
