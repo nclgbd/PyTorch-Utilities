f84d73b090c71ff336037fce7e2c328efffd0cc6
nguobadia <nicoleguob@gmail.com>
2021-07-31 22:18:19 -0700

    Creating packages

--- pytorch_vision_utils/__init__.py
+++ pytorch_vision_utils/__init__.py
@@ -1,7 +1,16 @@
 import os
 
+os.system("conda env update --name torch_base --file conda-envs/torch_base.yml")
+
+
+from . import avail_custom_models
+from .avail_custom_models import mobilenetv2
+from .avail_custom_models import xception
+from .avail_custom_models import avail_models
+
+from .mobilenetv2 import mobilenetv2, MobileNetV2
+from .xception import xception, Xception
+
 version='0.3.2'
 
-os.system("pip install -r requirements.txt")
-
-import pretrainedmodels
+print("Available models:\n", avail_models)
---
+++ pytorch_vision_utils/avail_custom_models/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+from . import mobilenetv2
+from . import xception
+
+avail_models = [mobilenetv2.__name__, xception.__name__]
