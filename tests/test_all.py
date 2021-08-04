e195ba1b5d9e272c93fd5fa1ef6508870635762a
nguobadia <nicoleguob@gmail.com>
2021-08-03 18:21:40 -0700

    Something's just wrong with my current environment. Tests will continue on the server

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,4 @@
 # from pytorch_vision_utils import pytorch_vision_utils
-from pytorch_vision_utils import Utilities
 from pytorch_vision_utils.Utilities import DataVisualizationUtilities, TrainingUtilities
 from pytorch_vision_utils.Utilities import clear_dirs, time_to_predict
 
