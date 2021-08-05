50857287f10078b947bbfad3c131cca5a726f694
nguobadia <nicoleguob@gmail.com>
2021-08-05 01:26:58 -0700

    Bump version: 0.3.5 → 0.3.6

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@
 pip uninstall pytorch_vision_utils -y
 ::bumpversion --dry-run --allow-dirty patch
 py -m build
-pip install dist/pytorch_vision_utils-0.3.5.tar.gz
+pip install dist/pytorch_vision_utils-0.3.6.tar.gz
