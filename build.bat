46780d36a699abe26a6791211ec78c7c32dd88c6
Nicole Gu <nicoleguob@gmail.com>
2021-08-12 00:46:25 -0700

    Bump version: 0.3.8 → 0.3.9

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@
 pip uninstall pytorch_vision_utils -y
 ::bumpversion --dry-run --allow-dirty patch
 py -m build
-pip install dist/pytorch_vision_utils-0.3.8.tar.gz
+pip install dist/pytorch_vision_utils-0.3.9.tar.gz
