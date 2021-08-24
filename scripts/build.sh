1cd247e065810a52bf5efb00e462e0bac72cd6e1
Nicole Gu <nicoleguob@gmail.com>
2021-08-23 17:54:13 -0700

    Changed method of initializing environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,8 @@ mkdir -p media/ incorrect_images/ saved_models/
 
 conda init bash
 
-export -f conda
-export -f __conda_activate
-export -f __conda_reactivate
-export -f __conda_hashr
+bash ~/.bashrc
+bash /opt/conda/etc/profile.d/conda.sh
 
-bash ~/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch_vision_dev
 pip install -e .
