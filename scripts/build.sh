63145de1d630fe03f6b8052afd190ea170ee34e0
Nicole Gu <nicoleguob@gmail.com>
2021-08-23 18:10:00 -0700

    Debugging

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,5 @@
 ls -la
 mkdir -p media/ incorrect_images/ saved_models/
 
-conda init bash
-
-bash ~/.bashrc
-bash /opt/conda/etc/profile.d/conda.sh
-
 conda activate pytorch_vision_dev
 pip install -e .
