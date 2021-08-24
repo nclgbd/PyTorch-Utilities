c9eb11449579d341b7d3863a9a30b0ea373a0103
Nicole Gu <nicoleguob@gmail.com>
2021-08-23 17:43:55 -0700

    Added exports to build file

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,13 @@
 ls -la
 mkdir -p media/ incorrect_images/ saved_models/
 
-set -euo pipefail
+conda init bash
+
+export -f conda
+export -f __conda_activate
+export -f __conda_reactivate
+export -f __conda_hashr
+
+bash ~/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch_vision_dev
 pip install -e .
