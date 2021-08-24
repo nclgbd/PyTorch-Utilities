91e91587405cd63a22b9480717b22c4b875a2ca7
Nicole Gu <nicoleguob@gmail.com>
2021-08-24 03:24:15 -0700

    Removed dirs from dockerignore file. Used absolute pathing when creating the additional directories.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash --login
 
+mkdir -p /workdir/media/ /workdir/incorrect_images/ /workdir/saved_models/ /workdir/test_data/ /workdir/data/
 conda activate pytorch_vision_dev
 pip install -e .
