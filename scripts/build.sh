505378a0c068b69613e113d7818c8df3140e307a
Nicole Gu <nicoleguob@gmail.com>
2021-09-13 00:58:30 -0700

    Trying to use build.sh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash --login
 
 mkdir -p /workdir/media/ /workdir/incorrect_images/ /workdir/saved_models/ /workdir/test_data/ /workdir/data/
-conda activate pytorch_vision_dev
-pip install -e .
