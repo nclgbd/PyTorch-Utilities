dafd05cd715cef7237dc0dbb1fde5185aff88cf0
Nicole Gu <nicoleguob@gmail.com>
2021-09-25 16:58:15 -0700

    Bump version: 0.3.15 → 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 print(find_packages())
 setup(
     name="pytorch_vision_utils",
-    version='0.3.15',
+    version='0.4.0',
     author="Nicole Gu",
     author_email="nicoleguob@gmail.com",
     description="PyTorch training and data visualization utilities",
